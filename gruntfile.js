73e70c49d0de06d1efb1cd38cc9e9182229dcb66
Andrew Barger <asbarg01@gmail.com>
2015-09-23 22:46:34 -0400

    Setting up skeleton for gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,10 @@
+﻿module.exports = function(grunt) {
+
+    //configure tasks
+    grunt.initConfig({});
+
+    //load plugins
+    grunt.loadNpmTasks( );
+
+    //register tasks
+    grunt.registerTask('default', []);
