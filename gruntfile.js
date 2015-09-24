58044508891e7a458f3a248f4ab0b434f0098f77
Andrew Barger <asbarg01@gmail.com>
2015-09-23 23:06:32 -0400

    configuring dev uglify task

--- gruntfile.js
+++ gruntfile.js
@@ -7,12 +7,23 @@
             build : {
                 src: 'app/components/js/*.js',
                 dest: 'app/app.min.js'
-                }
+                },
+            dev : {
+                options: {
+                    beautify : true,
+                    mangle : false,
+                    compress : false,
+                    preserveComments : 'all'
+                },
+                src : 'app/components/js/*.js',
+                dest : 'app/app.min.js'
             }
+        }
     });
 
     //load plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //register tasks
-    grunt.registerTask('default', ['uglify:build']);
+    grunt.registerTask('build', ['uglify:build']);
+    grunt.registerTask('dev', ['uglify:dev']);
