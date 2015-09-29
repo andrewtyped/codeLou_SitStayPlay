abeda77f6ef8c80df9f2b04158cbac520ab6d040
Andrew Barger <asbarg01@gmail.com>
2015-09-29 15:32:54 -0400

    set up a basic light box for navigating in a mobile view

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -1,1 +1,24 @@
-﻿
+﻿var app = {};
+
+app.lightBoxNav = function lightBoxNav() {
+    var navId = this.dataset.target;
+    var nav = document.querySelector(navId).cloneNode(true);
+    nav.id = 'lightBoxNav';
+
+    var lightBox = document.createElement('div');
+    lightBox.classList.add('light-box');
+    lightBox.appendChild(nav);
+    lightBox.addEventListener('click', function() {
+        this.parentElement.removeChild(this);
+    });
+
+    document.body.appendChild(lightBox);
+}
+
+app.bindEvents = function bindEvents() {
+    var navToggle = document.getElementsByClassName('nav-toggle')[0];
+
+    navToggle.addEventListener('click', app.lightBoxNav);
+}
+
+app.bindEvents();
