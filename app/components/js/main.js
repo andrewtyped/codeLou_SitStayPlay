b1efeba04c42294bfb3440c36b1a8e3fe427754e
Andrew Barger <asbarg01@gmail.com>
2015-10-01 18:56:19 -0400

    Adding an attractive fade animation to the lightbox navigation

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -9,10 +9,16 @@ app.lightBoxNav = function lightBoxNav() {
     lightBox.classList.add('light-box');
     lightBox.appendChild(nav);
     lightBox.addEventListener('click', function() {
-        this.parentElement.removeChild(this);
+        var that = this;
+        this.classList.add('fade-80-0');
+        setTimeout(function(){
+            
+            that.parentElement.removeChild(that);
+        },500);
     });
 
     document.body.appendChild(lightBox);
+    lightBox.classList.add('fade-0-80');
 }
 
 app.bindEvents = function bindEvents() {
