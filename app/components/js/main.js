5e82be1cd36e9795df9389bccb93f3f00342a461
Andrew Barger <asbarg01@gmail.com>
2015-10-10 23:47:54 -0400

    proof of concept for routing helper. Need to bind events for lightbox nav.

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -21,24 +21,32 @@ app.lightBoxNav = function lightBoxNav() {
     lightBox.classList.add('fade-0-80');
 }
 
+app.loadPartial = function loadPartial() {
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState === 4 && req.status === 200) {
+            var content = document.getElementById('content');
+            content.innerHTML = req.responseText;
+        }
+        else if (req.readyState === 4) {
+            alert('failed');
+        }
+    };
+
+    req.open('GET','views/' + this.dataset.partial + '.html');
+    req.send();
+}
+
 app.bindEvents = function bindEvents() {
     var navToggle = document.getElementsByClassName('nav-toggle')[0];
-
     navToggle.addEventListener('click', app.lightBoxNav);
+
+    var partialLinks = document.getElementsByClassName('partial-link');
+
+    for(var i = 0; i < partialLinks.length; i++) {
+        partialLinks[i].addEventListener('click', app.loadPartial);
+    }
 }
 
 app.bindEvents();
-
-var req = new XMLHttpRequest();
-req.onreadystatechange = function() {
-    if (req.readyState === 4 && req.status === 200) {
-        var content = document.getElementById('content');
-        content.innerHTML = req.responseText;
-    }
-    else if (req.readyState === 4) {
-        alert('failed');
-    }
-};
-
-req.open('GET','views/home.html');
-req.send();
